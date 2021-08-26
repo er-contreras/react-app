05aa5ca53129b8b559736907f8b29d2c17db7408
Erick Contreras <eri89--@hotmail.com>
2021-08-26 14:33:53 -0500

    Test operate

---
+++ src/__test__/operate.test.js
@@ -0,0 +1,24 @@
+import operate from '../logic/operate'
+const assert = require('assert')
+ 
+describe('Test sum function', () => {
+  it('It shoud be 3', () => {
+    assert(operate(1, 2, '+') === '3')
+  });
+
+  it('It shoud be -1', () => {
+    assert(operate(1, 2, '-') === '-1')
+  });
+
+  it('It shoud be 2', () => {
+    assert(operate(1, 2, 'x') === '2')
+  });
+
+  it('It shoud be 0.5', () => {
+    assert(operate(1, 2, '÷') === '0.5')
+  });
+
+  it('It shoud be 0', () => {
+    assert(operate(1, 2, '%') === '1')
+  });
+});
