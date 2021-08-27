61adf7709575776085da8c2629969a27bc13242d
Erick Contreras <eri89--@hotmail.com>
2021-08-27 17:53:37 -0500

    App test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
@@ -9,21 +9,23 @@ import Navbar from './components/Navbar';
 const App = () => (
   <>
     <Navbar />
-    <Switch>
-      <Route exact path="/home">
-        <Home />
-      </Route>
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
 
-      <Route path="/calculator">
-        <div className="App">
-          <Calculator />
-        </div>
-      </Route>
+        <Route path="/calculator">
+          <div className="App">
+            <Calculator />
+          </div>
+        </Route>
 
-      <Route path="/quote">
-        <Quote />
-      </Route>
-    </Switch>
+        <Route path="/quote">
+          <Quote />
+        </Route>
+      </Switch>
+    </Router>
   </>
 );
 
---
+++ src/__test__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import App from '../App';
+
+describe('App test', () => {
+  it('Should render same text passed into ...', () => {
+    const tree = TestRenderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
